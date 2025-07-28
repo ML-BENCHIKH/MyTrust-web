985b2f6f122804196b8840df6d1663c31aa4a2f4
ch.zoubir <chouaib.wzoubir@gmail.com>
2025-07-28 15:43:06 +0100

    feat(Acutualités), Ceci est un push pour ajouter un dossier de ffeauture acutalités de Chouaib a Mohamed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/features/Acceuil/Navbar";
+import Actuality from "@/features/actualites/Actuality";
 
 export default function Home() {
     return (
@@ -6,7 +7,7 @@ export default function Home() {
             <Navbar/>
             <main className="mt-32 w-full flex flex-col items-center justify-center min-h-[60vh]">
                 <div className="w-full max-w-4xl bg-gray-100 rounded-2xl shadow-inner p-12 flex flex-col items-center">
-                    <h1 className="text-3xl font-bold mb-4 text-gray-800">My Trust</h1>
+                   <Actuality/>
                 </div>
             </main>
         </>
---
+++ src/features/actualites/Actuality.tsx
@@ -0,0 +1,8 @@
+export default function Actuality() {
+    return (
+        <>
+            <h1 className="bg-amber-500">Chouaib</h1>
+            <h2> Anis</h2>
+        </>
+    );
+}
